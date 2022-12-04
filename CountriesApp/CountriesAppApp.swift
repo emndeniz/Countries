b31c4ecd7d9d845ad820c6a8fca12b1cf2193d12
Emin DENİZ <emndeniz@gmail.com>
2022-12-05 01:00:40 +0300

    Initial Commit

---
+++ CountriesApp/CountriesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesAppApp.swift
+//  CountriesApp
+//
+//  Created by Emin on 5.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct CountriesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
